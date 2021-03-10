d5fcafe2ca189f18dac1e9aa7dcb34c7f9ffec1e
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-11 00:35:04 +0530

    gql setup

--- user/schema.py
+++ user/schema.py
@@ -27,44 +27,5 @@ class Query(graphene.ObjectType):
         return user
 
 
-class CreateUser(graphene.Mutation):
-
-    new_user = graphene.Field(Users)
-
-    class Argumsnts:
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
-        email = graphene.String(required=True)
-        # name = graphene.String()
-        # gender = graphene.String()
-        # image = graphene.String()
-        # city = graphene.String()
-        # state = graphene.String()
-        # country = graphene.String()
-
-    def mutate(self, info, **kwargs):
-        if User.objects.filter(email=kwargs.get("email")).exists():
-            raise GraphQLError("Email already registered")
-
-        user = get_user_model()(
-            username=kwargs.get("username"),
-            email=kwargs.get("email"),
-        )
-        user.set_password(kwargs.get("password"))
-        user.save()
-
-        # name = kwargs.get("name")
-        # gender = kwargs.get("gender")
-        # image = kwargs.get("image")
-        # city = kwargs.get("city")
-        # state = kwargs.get("state")
-        # country = kwargs.get("country")
-
-        # UserProfile.objects.create(
-        #     user=user, name=name, city=city, state=state, country=country, image=image, gender=gender)
-
-        return CreateUser(new_user=user)
-
-
 class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
+    pass
