841ad90f72c51b31a4091fb46245eabefd0087ee
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-12 14:45:45 +0530

    idk++

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'graphene_django',
     'whitenoise.runserver_nostatic',
     'users',
+    'location'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -115,6 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 GRAPHENE = {
     'SCHEMA': 'backend.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
 
 # Internationalization
