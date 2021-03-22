6e55d0d17e1616f17a1c374d6181e56ba12d3bdf
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-22 16:21:13 +0530

    chat-system working

--- chats_id/models.py
+++ chats_id/models.py
@@ -1,15 +1,16 @@
+from users.models import UserProfile
 from django.db import models
 import uuid
-from django.conf import settings
+# from django.conf import settings
 # Create your models here.
 
 
 class ChatData(models.Model):
 
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    userA = models.ForeignKey(settings.AUTH_USER_MODEL,
+    userA = models.ForeignKey(UserProfile,
                               on_delete=models.CASCADE, related_name="first_user")
-    userB = models.ForeignKey(settings.AUTH_USER_MODEL,
+    userB = models.ForeignKey(UserProfile,
                               on_delete=models.CASCADE, related_name="second_user")
     collection = models.CharField(max_length=255, null=True, blank=True)
 
--- chats_id/schema.py
+++ chats_id/schema.py
@@ -1,5 +1,46 @@
+from users.models import UserProfile
 import graphene
-from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
 from .models import ChatData
 from graphql import GraphQLError
+from django.db.models import Q
+
+
+class Chat(DjangoObjectType):
+    class Meta:
+        model = ChatData
+
+
+class Query(graphene.ObjectType):
+    getChatIds = graphene.List(Chat)
+
+    def resolve_getChatIds(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError("Not Logged In!")
+
+        profile = UserProfile.objects.get(user=user)
+        return ChatData.objects.filter(Q(userA=profile) | Q(userB=profile))
+
+
+class CreateChat(graphene.Mutation):
+    ans = graphene.Field(Chat)
+
+    class Arguments:
+        id = graphene.String(required=True)
+        collection = graphene.String(required=True)
+
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError("Not Logged In!")
+        A = UserProfile.objects.get(user=user)
+        B = UserProfile.objects.get(id=kwargs.get("id"))
+        temp = ChatData.objects.create(
+            userA=A, userB=B, collection=kwargs.get("collection"))
+
+        return CreateChat(ans=temp)
+
+
+class Mutation(graphene.ObjectType):
+    create_chat = CreateChat.Field()
